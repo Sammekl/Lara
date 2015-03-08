ac518f1a09f18367377a3e08955574cee7ad4455
Samme <sammekleijn@gmail.com>
2015-03-08 21:34:45 +0100

    Exit prompt, Settings icon, Location

--- Android/RWS-Assistent/app/src/main/java/com/rwssistent/rws_assistent/activities/MainActivity.java
+++ Android/RWS-Assistent/app/src/main/java/com/rwssistent/rws_assistent/activities/MainActivity.java
@@ -1,18 +1,68 @@
 package com.rwssistent.rws_assistent.activities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.rwssistent.rws_assistent.R;
 
 
 public class MainActivity extends BaseActivity {
 
+    private TextView currentLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        currentLocation = (TextView) findViewById(R.id.current_location);
+        startLocationService();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    // ============================================
+    // Public Methods
+    // ============================================
+
+    // ============================================
+    // Private Methods
+    // ============================================
+
+    public void startLocationService() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                currentLocation.setText("Latitude: " + location.getLatitude() + ", Longitude: " + location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                Log.d("Latitude", "status");
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                Log.d("Latitude","enable");
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Log.d("Latitude","disable");
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
     }
 }
